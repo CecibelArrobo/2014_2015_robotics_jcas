bd4e4209d742e261f3294b29bab0cb1d41750565
ARROBO SARANGO JESSICA CECIBEL <100283869@l04sd206.aig.uc3m.es>
2014-10-29 12:10:02 +0100

    Updated

--- controllers/straight_compass/straight_compass.cpp
+++ controllers/straight_compass/straight_compass.cpp
@@ -1,9 +1,9 @@
 /**
- * @file    main.cpp
- * @brief   A simple example for maintaining a straight line with the compass.
+ * @file    straight_compass.cpp
+ * @brief   Robot go across the world in a straight line
  *
- * @author  Raul Perula-Martinez <raul.perula@uc3m.es>
- * @date    2014-07
+ * @author  Jessica Cecibel Arrobo Sarango <100283869@alumnos.uc3m.es>
+ * @date    2014-10
  */
 
 #include "MyRobot.h"
